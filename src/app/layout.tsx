6a1963a74bc8d7178246835e90c929226d3bfda2
kingsantus <nkingsantus@gmail.com>
2025-03-05 02:35:21 -0800

    merge with dev

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import PrelineScript from "./components/PrelineScript";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
+
+export const metadata: Metadata = {
+  title: "Flip Sonic",
+  description: "Powered By Solana SVM",
+  icons: {
+    icon: "/logo-desktop.png", // Default favicon
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+        <PrelineScript />
+      </body>
+    </html>
+  );
+}
