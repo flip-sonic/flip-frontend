7c80c2e85f9e14736228fc21ec53382eccd01fc4
kingsantus <nkingsantus@gmail.com>
2025-03-17 08:53:24 -0700

    check for error

--- src/app/components/swap/CreatePool.tsx
+++ src/app/components/swap/CreatePool.tsx
@@ -186,9 +186,7 @@ const CreatePool: FC<CreatePoolProps> = ({ tokens }) => {
             type="number"
             value={baseAmount}
             onChange={(e) => setBaseAmount(e.target.value)}
-            // className="outline-none border-none focus:ring-0 no-spinner text-tertiary bg-none  text-right"
-            className="bg-none border-none text-tertiary text-right"
-            placeholder="0.00" required
+            placeholder="0.00" className="outline-none border-none focus:ring-0 no-spinner text-tertiary bg-none  text-right" required
           />
         </div>
         {/* </div> */}
@@ -224,8 +222,8 @@ const CreatePool: FC<CreatePoolProps> = ({ tokens }) => {
             type="number"
             value={quoteAmount}
             onChange={(e) => setQuoteAmount(e.target.value)}
-            // className="outline-none border-none focus:ring-0 no-spinner text-tertiary bg-none text-right"
-            className="bg-none border-none text-tertiary text-right"
+            className="outline-none border-none focus:ring-0 no-spinner text-tertiary bg-none text-right"
+            // className="bg-none border-none text-tertiary text-right"
             placeholder="0.00" required
           />
         </div>
@@ -272,13 +270,13 @@ const CreatePool: FC<CreatePoolProps> = ({ tokens }) => {
       </div>
 
       {/* Lock Switch */}
-      {/* <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3">
         <Switch checked={isLocked} onCheckedChange={setIsLocked} className="" />
         <div className="flex items-center space-x-2">
           <span className="text-white text-sm">Lock</span>
           <InfoCircle className="w-4 h-4 text-secondary" />
         </div>
-      </div> */}
+      </div>
 
       {/* Create and Deposit Button */}
       <Button className="w-full bg-secondary rounded-[10px] h-[51px]" onClick={handleCreatePool}>{appLoading ? "loading" : "Create and deposit"}        
